87544d4daa75e47d7fe409bcee45c38f989e4b7a
yashubajaj28 <55505231+yashubajaj28@users.noreply.github.com>
2019-10-01 22:37:10 +0530

    1

---
+++ EXP4_SMART_PHONE_CONTROLLED_SYSTEM_TO_CONTROL_LIGHT/CODING.ino
@@ -0,0 +1,25 @@
+char data=0;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(13,OUTPUT);
+}
+
+void loop() {
+
+  // put your main code here, to run repeatedly:
+if(Serial.available() > 0)
+{
+data=Serial.read();
+Serial.println(data);    
+if(data=='1')
+{
+digitalWrite(13,HIGH);
+
+  }
+else 
+{
+  digitalWrite(13,LOW);
+}
+  
+}
